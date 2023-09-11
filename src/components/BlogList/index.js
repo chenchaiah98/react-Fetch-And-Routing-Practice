a4d799b1125be1be074687951bce86cc4faa185b
rchenchaiah98 <rchenchaiah98@gmail.com>
2023-09-11 06:44:52 +0000

    changes blog code

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,6 +1,11 @@
 // Write your JS code here
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
 import BlogItem from '../BlogItem'
+
+import './index.css'
 
 class BlogList extends Component {
   state = {blogList: [], isLoading: true}
@@ -31,9 +36,9 @@ class BlogList extends Component {
     return (
       <div>
         {isLoading ? (
-          <p>loaing</p>
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
         ) : (
-          <ul>
+          <ul className="list">
             {blogList.map(e => (
               <li key={e.id}>
                 <BlogItem eachBlog={e} />
